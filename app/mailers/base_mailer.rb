be9150d853afbf39502f4a0062a64db73b17237b
Chad Rosen <chadrosen@gmail.com>
2010-11-18 16:50:04 -0800

    fix return path

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -2,5 +2,5 @@ class BaseMailer < ActionMailer::Base
 
   helper :application
         
-  default :from => OPTIONS[:email_from], :return_path => OPTIONS[:return_path]
+  default :from => OPTIONS[:email_from], :return_path => OPTIONS[:email_return_path]
 end
