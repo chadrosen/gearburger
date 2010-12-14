c0586c489e98c7fcf3c8bf56dec53bb6f0f7e722
Chad Rosen <chadrosen@gmail.com>
2010-12-13 18:45:43 -0800

    only do the compass compile in dev

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,7 @@
-require 'compass'
-require 'compass/app_integration/rails'
-Compass::AppIntegration::Rails.initialize!
+# Turn on rails compass integration for dev mode only. 
+# This auto-compiles the stylesheets from app/stylesheets to public/stylesheets
+if Rails.env == "development"
+  require 'compass'
+  require 'compass/app_integration/rails'
+  Compass::AppIntegration::Rails.initialize!
+end
