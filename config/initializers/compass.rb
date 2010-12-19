e3b526c8cfff4b307607667c9dc3a437ab104386
Chad Rosen <chadrosen@gmail.com>
2010-12-19 15:17:24 -0800

    attempt to bypass the stupid error

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
-# Turn on rails compass integration for dev mode only. 
+# Turn on rails compass integration for dev mode only (IE not heroky)
 # This auto-compiles the stylesheets from app/stylesheets to public/stylesheets
-if Rails.env == "development"
+unless ENV["RAILS_ENV"]
   require 'compass'
   require 'compass/app_integration/rails'
   Compass::AppIntegration::Rails.initialize!
