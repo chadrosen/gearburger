99016a536ade8b3111ad4ab80778436732321a30
Chad Rosen <chadrosen@gmail.com>
2010-12-19 15:58:15 -0800

    still trying

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,10 +1,7 @@
 # Turn on rails compass integration for dev mode only (IE not heroky)
 # This auto-compiles the stylesheets from app/stylesheets to public/stylesheets
 
-if Rails.env.production?
-  Compass.configuration.sass_options = { :never_update => true }
-  Sass::Plugin.options[:never_update] = true 
-else
+if Rails.env.development?
   require 'compass'
   require 'compass/app_integration/rails'
   Compass::AppIntegration::Rails.initialize!
