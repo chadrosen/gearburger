c57248fc90f3b1ef9bb8d8ddcce072e0d4288486
Chad Rosen <chadrosen@gmail.com>
2010-12-19 15:31:41 -0800

    try again with the compass

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 # Turn on rails compass integration for dev mode only (IE not heroky)
 # This auto-compiles the stylesheets from app/stylesheets to public/stylesheets
-unless ENV["RAILS_ENV"]
+if ENV["RAILS_ENV"] == "development"
   require 'compass'
   require 'compass/app_integration/rails'
   Compass::AppIntegration::Rails.initialize!
