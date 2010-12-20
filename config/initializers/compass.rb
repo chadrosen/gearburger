233eea87209185121113ce500e7642b575d135a0
Chad Rosen <chadrosen@gmail.com>
2010-12-19 20:07:18 -0800

    sigh again and again

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,8 @@
 # Turn on rails compass integration for dev mode only (IE not heroky)
 # This auto-compiles the stylesheets from app/stylesheets to public/stylesheets
-require 'compass'
-require 'compass/app_integration/rails'
-Compass::AppIntegration::Rails.initialize!
+# Only use the compass stuff in dev
+if Rails.env.development?
+  require 'compass'
+  require 'compass/app_integration/rails'
+  Compass::AppIntegration::Rails.initialize!
+end
