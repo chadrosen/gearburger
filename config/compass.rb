3993e7d7907c2987b1e5c040f4a88156ad61f8fa
Chad Rosen <chadrosen@gmail.com>
2010-12-19 16:56:44 -0800

    sigh

--- config/compass.rb
+++ config/compass.rb
@@ -7,10 +7,9 @@ css_dir = "public/stylesheets" # Compile stylesheets to the public/stylesheets d
 sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 
-Sass::Plugin.options[:never_update] = true
-Compass.configuration.sass_options = { :never_update => true }
-
 puts "Compass install"
-#puts Sass::Plugin.options[:never_update]
+
+puts Compass.configuration.sass_options
+Compass.configuration.sass_options = { :never_update => true }
 puts Compass.configuration.sass_options
 puts "Compass post install"
