154093103b6aae9173c4b4589b120f0ae35ecd1d
Chad Rosen <chadrosen@gmail.com>
2010-12-19 16:48:40 -0800

    sigh

--- config.ru
+++ config.ru
@@ -1,6 +1,4 @@
 # This file is used by Rack-based servers to start the application.
 require ::File.expand_path('../config/environment',  __FILE__)
 
-Sass::Plugin.options[:never_update] = true
-
 run Gearburger::Application
--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,19 @@
 # Turn on rails compass integration for dev mode only (IE not heroky)
 # This auto-compiles the stylesheets from app/stylesheets to public/stylesheets
-require 'compass'
-require 'compass/app_integration/rails'
-Compass::AppIntegration::Rails.initialize!
+if ENV["RAILS_ENV"] == "development"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+  puts "compass install"
+
+  require 'compass'
+  require 'compass/app_integration/rails'
+  Compass::AppIntegration::Rails.initialize!
+end
