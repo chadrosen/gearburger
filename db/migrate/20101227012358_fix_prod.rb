8c761e6dbe10dd69856754e3a9acf69cdc3c7060
Chad Rosen <chadrosen@gmail.com>
2010-12-26 17:25:03 -0800

    attempt to patch prod db

---
+++ db/migrate/20101227012358_fix_prod.rb
@@ -0,0 +1,16 @@
+class FixProd < ActiveRecord::Migration
+  def self.up
+    
+    drop_table(:product_prices)
+    drop_table(:alternatives)
+    drop_table(:experiments)
+    remove_column(:products, :large_image_url)
+    remove_column(:products, :manufacturer_id)
+    drop_table(:products_users)
+    remove_column(:user_product_emails, :products_users_count)
+
+  end
+
+  def self.down
+  end
+end
