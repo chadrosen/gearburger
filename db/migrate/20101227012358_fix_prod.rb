28e175c82dab007cb10fb8580f348bcb8d17d25f
Chad Rosen <chadrosen@gmail.com>
2010-12-26 17:39:36 -0800

    no pp

--- db/migrate/20101227012358_fix_prod.rb
+++ db/migrate/20101227012358_fix_prod.rb
@@ -1,11 +1,6 @@
 class FixProd < ActiveRecord::Migration
   def self.up
-    
-    remove_column(:products, :large_image_url)
-    remove_column(:products, :manufacturer_id)
-    drop_table(:products_users)
     remove_column(:user_product_emails, :products_users_count)
-
   end
 
   def self.down
