91d6c7694708b58d58539a529e14e36ca0c4913b
Chad Rosen <crosen@twitter.com>
2011-01-01 18:00:27 -0800

    config delayed jobs a bit

--- config/initializers/delayed_jobs_init.rb
+++ config/initializers/delayed_jobs_init.rb
@@ -18,8 +18,10 @@ class ActiveRecord::Base
   end 
 end
 
-# Delayed::Job.destroy_failed_jobs = false
-#silence_warnings do
-#  Delayed::Job.const_set("MAX_ATTEMPTS", 3)
-#  Delayed::Job.const_set("MAX_RUN_TIME", 15.minutes)
-#end
+# Don't delete jobs that failed. Keep them in there so I can debug
+Delayed::Worker.destroy_failed_jobs = false
+
+silence_warnings do
+  Delayed::Job.const_set("MAX_ATTEMPTS", 3)
+  Delayed::Job.const_set("MAX_RUN_TIME", 2.hours)
+end
